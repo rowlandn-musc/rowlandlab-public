ca0df76f905f08053d21cc8b7620639fbac2c808
rowlandn-musc <68341688+rowlandn-musc@users.noreply.github.com>
2021-12-23 22:38:59 -0500

    initial upload

--- VR_functions/calculateVRMetrics.m
+++ VR_functions/calculateVRMetrics.m
@@ -1,6 +1,6 @@
 % shade option= 0(no) or 1 (yes)
 
-
+helloaa
 function [vrMetrics,rejecttrials,reachstarttime] = calculateVRMetrics(vrData,environmentSettings,figureHandles,colors,ls,shadeoption)
 
 % initialize sphere radiuses
@@ -21,11 +21,11 @@ sp2 = makehgtform('xrotate',alpha*pi/180)*sp;
 startPosition = sp2(1:3)';
 
 if strcmp(vrData.information.studyCondition,'Multiple reaches per cue')
-    
+
     % get target positions
     environmentTargetPositions = [str2double(environmentSettings.target1.position.x) str2double(environmentSettings.target1.position.y) str2double(environmentSettings.target1.position.z)
         str2double(environmentSettings.target2.position.x) str2double(environmentSettings.target2.position.y) str2double(environmentSettings.target2.position.z)];
-    
+
     % rotate target positions
     for i = 1:2
         tp = [environmentTargetPositions(i,:)';1];
@@ -33,7 +33,7 @@ if strcmp(vrData.information.studyCondition,'Multiple reaches per cue')
         environmentTargetPositions(i,:) = tp2(1:3)';
     end
 end
-                          
+
 % get events pertaining to first reach of each sequence
 atStartPosition = vrData.events.atStartPosition.time;
 cueEvent = vrData.events.cueEvent.time;
@@ -64,15 +64,15 @@ targetUpData = targetUpData(firstReachInd);
 targetPositions = zeros(length(firstReachInd),3);
 usedHand = cell(length(firstReachInd),3);
 for i = 1:length(firstReachInd)
-    
+
     if strcmp(vrData.information.studyCondition,'Multiple reaches per cue')
         targetPositions(i,:) = environmentTargetPositions(vrData.events.targetHit.data(firstReachInd(i)).targetNumber,:);
     elseif strcmp(vrData.information.studyCondition,'Single reach per cue, random time') || strcmp(vrData.information.studyCondition,'Single reach per cue, random time, no visual cue')
-        
+
         p = [targetUpData(i).targetPosition'; 1];
         p2 = makehgtform('xrotate',alpha*pi/180)*p;
         targetPositions(i,:) = p2(1:3)';
-        
+
     else
         targetPositions(i,:) = NaN(1,3);
     end
@@ -114,26 +114,26 @@ V_plot = [];
 rejecttrials=nan;
 
 for i = 1:length(reaches)
-    
+
     %% at start position waiting for target to appear
     ind = (vrData.tracker.time>=reaches(i).atStartPosition) & (vrData.tracker.time<=(reaches(i).cueEvent));
-        
+
     x = vrData.tracker.p.(reaches(i).testedSide)(ind,1);
     y = vrData.tracker.p.(reaches(i).testedSide)(ind,2);
     z = vrData.tracker.p.(reaches(i).testedSide)(ind,3);
-    
+
     vx = vrData.tracker.v.(reaches(i).testedSide)(ind,1);
     vy = vrData.tracker.v.(reaches(i).testedSide)(ind,2);
     vz = vrData.tracker.v.(reaches(i).testedSide)(ind,3);
     V = sqrt(vx.^2+vy.^2+vz.^2);
-    
+
     ax = vrData.tracker.a.(reaches(i).testedSide)(ind,1);
     ay = vrData.tracker.a.(reaches(i).testedSide)(ind,2);
     az = vrData.tracker.a.(reaches(i).testedSide)(ind,3);
     A = sqrt(ax.^2+ay.^2+az.^2);
-    
+
     d_start = sqrt(sum(([x y z] - repmat(reaches(i).startPosition,length(x),1)).^2,2));
-    
+
     % skip if mean distance from start exceeds 8 cm
     if mean(d_start) >= 0.08 & strcmp(vrData.information.env,'3')
         vrMetrics.IOC(i) = NaN;
@@ -149,31 +149,31 @@ for i = 1:length(reaches)
         vrMetrics.avgVelocity(i) = NaN;
         vrMetrics.avgAcceleration(i) = NaN;
         vrMetrics.maxAcceleration(i)= NaN;
-        
+
         rejecttrials=[rejecttrials i];
         reachstarttime(i)=nan;
         continue
     end
-    
+
     %% at start position waiting for go
     ind = (vrData.tracker.time>=reaches(i).cueEvent) & (vrData.tracker.time<=(reaches(i).goEvent));
-        
+
     x = vrData.tracker.p.(reaches(i).testedSide)(ind,1);
     y = vrData.tracker.p.(reaches(i).testedSide)(ind,2);
     z = vrData.tracker.p.(reaches(i).testedSide)(ind,3);
-    
+
     vx = vrData.tracker.v.(reaches(i).testedSide)(ind,1);
     vy = vrData.tracker.v.(reaches(i).testedSide)(ind,2);
     vz = vrData.tracker.v.(reaches(i).testedSide)(ind,3);
     V = sqrt(vx.^2+vy.^2+vz.^2);
-    
+
     ax = vrData.tracker.a.(reaches(i).testedSide)(ind,1);
     ay = vrData.tracker.a.(reaches(i).testedSide)(ind,2);
     az = vrData.tracker.a.(reaches(i).testedSide)(ind,3);
     A = sqrt(ax.^2+ay.^2+az.^2);
-    
+
     d_start = sqrt(sum(([x y z] - repmat(reaches(i).startPosition,length(x),1)).^2,2));
-    
+
     % skip if distance from start (towards end of wait time) exceeds 8 cm
     if mean(d_start(end-10:end)) >= 0.08 && strcmp(vrData.information.env,'3')
         vrMetrics.IOC(i) = NaN;
@@ -189,47 +189,47 @@ for i = 1:length(reaches)
         vrMetrics.avgVelocity(i) = NaN;
         vrMetrics.avgAcceleration(i) = NaN;
         vrMetrics.maxAcceleration(i)= NaN;
-        
+
         rejecttrials=[rejecttrials i];
         reachstarttime(i)=nan;
-        
+
         continue
     end
-    
+
     %% from go to shortly after target hit
     ind = (vrData.tracker.time>=reaches(i).goEvent) & (vrData.tracker.time<=(reaches(i).targetHit+0.5));
-    
+
     t = vrData.tracker.time(ind);
-    
+
     x = vrData.tracker.p.(reaches(i).testedSide)(ind,1);
     y = vrData.tracker.p.(reaches(i).testedSide)(ind,2);
     z = vrData.tracker.p.(reaches(i).testedSide)(ind,3);
-    
+
     vx = vrData.tracker.v.(reaches(i).testedSide)(ind,1);
     vy = vrData.tracker.v.(reaches(i).testedSide)(ind,2);
     vz = vrData.tracker.v.(reaches(i).testedSide)(ind,3);
     V = sqrt(vx.^2+vy.^2+vz.^2);
-    
+
     ax = vrData.tracker.a.(reaches(i).testedSide)(ind,1);
     ay = vrData.tracker.a.(reaches(i).testedSide)(ind,2);
     az = vrData.tracker.a.(reaches(i).testedSide)(ind,3);
     A = sqrt(ax.^2+ay.^2+az.^2);
-    
+
     d_start = sqrt(sum(([x y z] - repmat(reaches(i).startPosition,length(x),1)).^2,2));
     d_target = sqrt(sum(([x y z] - repmat(reaches(i).targetPosition,length(x),1)).^2,2));
-    
+
     % define reach end as time of smallest distance to target
     indReachEnd = find(d_target == min(d_target),1,'first');
     reachEnd = t(indReachEnd);
-    
+
     % find time when hand sphere first exits start sphere
     indStartExit = find(d_start>(startPositionRadius+handSphereRadius),1,'first');
     tStartExit = t(indStartExit);
-    
+
     % find velocity peak
     vMax = max(V(max(1,(indStartExit-10)):indReachEnd));
     vMin = min(V(1:indStartExit));
-    
+
     % define reach start as time closest to sphere exit when the hand
     % velocity rises above 10% of max
     th = vMin + (vMax-vMin)*0.1;
@@ -240,51 +240,51 @@ for i = 1:length(reaches)
         indReachStart = find(t<=tStartExit & V<th,1,'last')+1;
         reachStart = t(indReachStart);
     end
-    
+
     % reach indices
     indReach = indReachStart:indReachEnd;
-    
+
     % calculate jerk
     dax = ax(3:end) - ax(1:end - 2);
     day = ay(3:end) - ay(1:end - 2);
     daz = az(3:end) - az(1:end - 2);
     dt = t(3:end) - t(1:end - 2);
-    
+
     jx = dax./dt; jx = [jx(1); jx; jx(end)];
     jy = day./dt; jy = [jy(1); jy; jy(end)];
     jz = daz./dt; jz = [jz(1); jz; jz(end)];
-    
+
     J = sqrt(jx.^2 + jy.^2 + jz.^2); % magnitude of jerk
-    
+
     % extract movement data
     Pr = [x(indReach) y(indReach) z(indReach)];
     Vr = [vx(indReach) vy(indReach) vz(indReach)];
     Ar = [ax(indReach) ay(indReach) az(indReach)];
     Jr = [jx(indReach) jy(indReach) jz(indReach)];
-    
+
     % calculate IoC
     pStart = [x(indReachStart) y(indReachStart) z(indReachStart)];
     pEnd = [x(indReachEnd) y(indReachEnd) z(indReachEnd)];
     straightLineDistance = norm(pEnd-pStart);
     handPathLength = sum(sqrt(sum(diff([x(indReach) y(indReach) z(indReach)]).^2,2)));
     IOC = handPathLength/straightLineDistance;
-    
+
     % calculate maxumum velocity
     [maxVelocity, indMaxV] = max(V(indReach));
     indMaxV = indMaxV + indReachStart-1;
-    
+
     % calculate movement duration
     movementDuration = reachEnd-reachStart;
-    
+
     % calculate reaction time
     reactionTime = reachStart-reaches(i).goEvent;
-    
+
     % calculate time from start to peak velocity, normalized by movement
     % time
     maxVTime = t(indMaxV);
     timeToMaxVelocity = maxVTime-reachStart;
     timeToMaxVelocity_n = timeToMaxVelocity/movementDuration*100;
-    
+
     % calculate number of velocity peaks
     n_mov = pEnd'-pStart'; n_mov = n_mov/norm(n_mov);
     handAccelForPeaks = (Ar*n_mov);
@@ -295,26 +295,26 @@ for i = 1:length(reaches)
     transition(above) = 1;
     transition(below) = -1;
     transition(transition==0) = [];
-    
+
     velocityPeaks = sum(diff(transition)<0);
-    
+
     % normalized jerk
     [Pmj, Vmj, Amj, Jmj] = generateMinJerkTrajectory(Pr,Vr,Ar,Jr,t(indReach));
     normalizedJerk = trapz(t(indReach),sqrt(sum(Jr.^2,2))) / trapz(t(indReach),sqrt(sum(Jmj.^2,2)));
-    
+
     % precision
     accuracy = 1/min(d_target);
-    
+
     % average velocity
     avgVelocity=handPathLength/movementDuration;
-    
+
     % average acceleration
     avgAcceleration=mean(A);
-    
+
     % max acceleration
     maxAcceleration=max(A);
-    
-    
+
+
     % save results
     vrMetrics.IOC(i) = IOC;
     vrMetrics.maxVelocity(i) = maxVelocity;
@@ -329,9 +329,9 @@ for i = 1:length(reaches)
     vrMetrics.avgVelocity(i) =avgVelocity;
     vrMetrics.avgAcceleration(i) = avgAcceleration;
     vrMetrics.maxAcceleration(i)= maxAcceleration;
-    
+
     reachstarttime(i)=reachStart;
-    
+
     % generate plot data
     t_reach = t(indReach);
     t_plot = linspace(t_reach(1),t_reach(end),500)';
@@ -339,7 +339,7 @@ for i = 1:length(reaches)
     y_plot = [y_plot interp1(t_reach,y(indReach),t_plot)];
     z_plot = [z_plot interp1(t_reach,z(indReach),t_plot)];
     V_plot = [V_plot interp1(t_reach,V(indReach),t_plot)];
-    
+
 end
 
 % plot graphs
@@ -408,5 +408,3 @@ else
     ylabel('|V| [m/s]')
     xlabel('% movement []')
 end
-
-    
